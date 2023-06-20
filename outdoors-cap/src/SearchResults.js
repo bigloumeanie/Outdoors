aa51393e698a064fcb7732d6680af3ae048a0b9f
ColinMcHugh <colinmchugh87@gmail.com>
2023-06-20 13:56:02 -0400

    on my bs again

---
+++ outdoors-cap/src/SearchResults.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import nationalParksData from "./assets/data/nationalparks.json";
+
+function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    setSearchResults(nationalParksData.Table);
+  }, []);
+
+  return (
+    <div>
+      <h3>Search Results:</h3>
+      {searchResults.length === 0 ? (
+        <p>No results found.</p>
+      ) : (
+        <div>
+          {" "}
+          {/* Wrap the map expression in a div */}
+          {searchResults.map((park) => (
+            <div key={park.LocationID}>
+              <h4>{park.LocationName}</h4>
+              <p>Address: {park.Address}</p>
+              <p>City: {park.City}</p>
+              <p>State: {park.State}</p>
+              <p>Zip Code: {park.ZipCode}</p>
+              <p>Phone: {park.Phone}</p>
+              <p>Fax: {park.Fax}</p>
+              <p>
+                Visit Website:{" "}
+                <a href={park.Visit} target="_blank" rel="noopener noreferrer">
+                  {park.Visit}
+                </a>
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default SearchResults;
--- outdoors-cap/src/nationalParksSearch.js
+++ outdoors-cap/src/nationalParksSearch.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import locationsData from "./assets/data/locations.json";
+import parkTypesData from "./assets/data/parktypes.json";
+import SearchResults from "./SearchResults";
 
 function NationalParksSearch() {
   const [searchBy, setSearchBy] = useState("location");
   const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedParkType, setSelectedParkType] = useState("");
 
   const handleSearchByChange = (e) => {
     setSearchBy(e.target.value);
@@ -13,12 +16,17 @@ function NationalParksSearch() {
     setSelectedLocation(e.target.value);
   };
 
+  const handleParkTypeChange = (e) => {
+    setSelectedParkType(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle the search submission, you can add your logic here
     console.log("Submitted!");
     console.log("Search By:", searchBy);
     console.log("Selected Location:", selectedLocation);
+    console.log("Selected Park Type:", selectedParkType);
   };
 
   return (
@@ -52,8 +60,29 @@ function NationalParksSearch() {
           </div>
         )}
 
+        {searchBy === "parkType" && (
+          <div>
+            <label>
+              Park Type:
+              <select value={selectedParkType} onChange={handleParkTypeChange}>
+                <option value="">Select a park type</option>
+                {parkTypesData.map((parkType) => (
+                  <option key={parkType} value={parkType}>
+                    {parkType}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        )}
+
         <button type="submit">Search</button>
       </form>
+
+      <SearchResults
+        selectedLocation={selectedLocation}
+        selectedParkType={selectedParkType}
+      />
     </div>
   );
 }
