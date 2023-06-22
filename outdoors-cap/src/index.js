f467b77eee384d080583d7685495886cd68fcd65
bigloumeanie <louielacarbonara@Gmail.com>
2023-06-22 11:42:44 -0400

    cleanup

--- outdoors-cap/src/index.js
+++ outdoors-cap/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Button from "react-bootstrap/Button"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
